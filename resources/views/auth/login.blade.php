c23a8330d3457191f3d883a7c6677252611a9547
Erik <erikondra@yahoo.de>
2021-09-13 08:37:19 +0200

    error adjustment

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,17 +14,11 @@
                                     <input type="text" placeholder="Email" id="email" class="form-control" name="email"
                                            required
                                            autofocus>
-                                    @if ($errors->has('email'))
-                                        <span class="text-danger">{{ $errors->first('email') }}</span>
-                                    @endif
                                 </div>
 
                                 <div class="form-group mb-3">
                                     <input type="password" placeholder="Password" id="password" class="form-control"
                                            name="password" required>
-                                    @if ($errors->has('password'))
-                                        <span class="text-danger">{{ $errors->first('password') }}</span>
-                                    @endif
                                 </div>
 
                                 <div class="form-group mb-3">
